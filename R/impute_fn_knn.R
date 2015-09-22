4449d7b306dae8fedef1b8295a9c334245cf6e23
Alex Whitworth <awhitworth@dandb.com>
2015-09-22 16:14:40 -0700

    remove kernelMatrix from code

--- R/impute_fn_knn.R
+++ R/impute_fn_knn.R
@@ -13,8 +13,24 @@ impute_fn_knn <- function(values, distances, k, kern) {
   smallest_distances <- distances[ranks][1:k]
   knn_values <- values[ranks][1:k]
   # calculate weights
-  d <- kernelMatrix(kern, c(0, smallest_distances))[1, -1, drop= TRUE]
+  # d <- kernelMatrix(kern, c(0, smallest_distances))[1, -1, drop= TRUE]
+  # 9/22: rather than use kernlab, calculate kernels by hand
+  d <- kern_wt(kern, c(0, smallest_distances))
   knn_weights <- d / sum(d)
   
   weighted.mean(knn_values, knn_weights)
 }
+
+
+# this function is equivalent to calculating via
+# kernelMatrix(kern, c(0, smallest_distances))[1, -1, drop= TRUE]
+kern_wt <- function (kernel, x) {
+  if (is(x, "vector")) x <- as.matrix(x)
+  
+  sigma = kpar(kernel)$sigma
+  n <- dim(x)[1]
+  dota <- rowSums(x * x)/2
+  return(exp(2 * sigma * (-dota))[-1])
+}
+  
+ 
